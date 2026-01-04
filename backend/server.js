10b0b79dc27c1bfe6fc61fd65094f69ef1d9d62a
Archit Kamble <architkamble001@gmail.com>
2026-01-04 22:48:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -69,3 +69,7 @@ app.get('*',(req,res)=>{
 app.listen(process.env.PORT||4000,'0.0.0.0',()=>{
   console.log('Server running');
 });
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log('Server running on port', PORT);
+});
